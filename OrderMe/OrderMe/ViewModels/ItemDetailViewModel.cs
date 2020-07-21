49802469f759080d8c6e7b95ca3e4639bded96eb
Enrique Cedeno <ecedeno@phoenixcalibrationdr.com>
2020-07-21 19:56:04 -0400

    Creating Proyect

---
+++ OrderMe/OrderMe/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using OrderMe.Models;
+
+namespace OrderMe.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
