e0471f0ad720e2fe95859c55a67498113bd692c9
Enrique Cedeno <ecedeno@phoenixcalibrationdr.com>
2020-07-22 18:34:18 -0400

    Prueba Para que vean

--- OrderMe/OrderMe.Android/MainActivity.cs
+++ OrderMe/OrderMe.Android/MainActivity.cs
@@ -28,6 +28,6 @@ namespace OrderMe.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+        } 
     }
 }
